d28c6f2dedeec56e418bbb5948245d5865276df5
ClayData <claytonortiz96@gmail.com>
2020-03-13 19:56:54 -0500

    filled out server.js

--- server.js
+++ server.js
@@ -34,17 +34,58 @@ connection.connect(function(err) {
 
 
 app.get("/", function(req, res){
+    connection.query("SELECT * FROM movies;", function(err, data) {
+        if(err) {
+            return res.status(500).end();
+        };
+
+        res.render("index", { movies: data });
+    });
+});
+
+app.post("/api/movies", function(req, res){
+    connection.query("INSERT INTO movies (movie) VALUES (?)", [req.body.movie], function(err, result) {
+        if (err) {
+            return res.status(500).end();
+        };
+        
+        res.json({ id: result.insertId });
+        console.log({ id: result.insertId });
+    })
+});
+
+app.put("/api/movies/:id", function(req, res){
+    connection.query("UPDATE movies SET movie = ? WHERE id = ?", [req.body.movie, req.params.id], function(err, result) {
+        if (err) {
+          // If an error occurred, send a generic server failure
+          return res.status(500).end();
+        }
+        else if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
     
+      });
 });
 
-app.post("/movies", function(req, res){
-
-});
-
-app.put("/movies/:id", function(req, res){
+app.delete("/api/movies/:id", function(req, res){
+    connection.query("DELETE FROM movies WHERE id = ?", [req.params.id], function(err, result) {
+        if (err) {
+          // If an error occurred, send a generic server failure
+          return res.status(500).end();
+        }
+        else if (result.affectedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
     
-});
-
-app.delete("/movies/:id", function(req, res){
+      });
+    });
     
+    // Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+      console.log("Server listening on: http://localhost:" + PORT);
 });
+    
